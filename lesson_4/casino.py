554dac7948c5c3e02dd6597bf288afe2adec7196
Pe4enushko <mr.stone9999@gmail.com>
2023-06-14 01:50:40 +0400

    casino love

--- lesson_4/casino.py
+++ lesson_4/casino.py
@@ -1,10 +1,27 @@
 import random
 guess = 0
 randnum = random.randint(0,100)
+interrupted = False
+
 while guess != randnum:
     try:
+        # Не повторять же постоянно больше или меньше
+        if not interrupted:
+            print(f"Чуть {'меньше' if guess > randnum else 'больше'}")
+        else:
+            interrupted = False
+            
         guess = int(input("Угадаешь? Уверен, что нет. "), 0)
+
+    except KeyboardInterrupt as e:
+        answer = input("А ты точно ливнуть хочешь? QwQ ").lower()
+        interrupted = True
+        
+        if answer == "да":
+            exit()
+        else:
+            print("Тогда продолжим AwA")
     except:
         print("Дурак?")
 else:
-    print("Ле....")
+    print("Что-ж, победа.")

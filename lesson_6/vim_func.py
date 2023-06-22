ebe85b1f907c770030044b42446aba1a3193f1f1
Pe4enushko <mr.stone9999@gmail.com>
2023-06-22 23:49:43 +0400

    vim fix

--- lesson_6/vim_func.py
+++ lesson_6/vim_func.py
@@ -1,4 +1,6 @@
 def daw(txt, i, words_count):
+    if(words_count <= 0):
+        return txt
     selected_word = get_selected_word_index(txt, i)
     words = txt.split(' ')
     final_words = words[:selected_word] + words[selected_word + words_count:]
@@ -12,6 +14,6 @@ def get_selected_word_index(txt, cursor_position):
     return left_words_count - 1
     
 # test
-inp = input('gimmie test 3 args')
+inp = input('Дай мне 3 аргумента через запятую: текст, позицию, кол-во слов')
 args = inp.split(',')
 print(daw(args[0], int(args[1]), int(args[2])))
